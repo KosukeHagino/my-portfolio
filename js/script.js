d678c5e0a6b76066e5e83072f5651890740a2646
Kosuke_Hagino <ha_28_co@gmail.com>
2025-12-24 10:53:31 +0900

    ヘッダー、ハンバーガメニューを作成

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+/**************************************************
+ハンバーガーメニュー
+**************************************************/
+
+// クラスをトグルするターゲット要素を取得
+const menuBtn = document.querySelector('#menu');
+const globalNav = document.querySelector('#global_nav');
+const mask = document.querySelector('#mask');
+
+// クリックのトリガーとなる全ての要素を取得
+const triggers = document.querySelectorAll('#menu, #global_nav_item a, #mask');
+
+// 共通の処理を関数として定義
+const toggleMenu = () => {
+
+    // 3つのターゲット要素のクラスを同時にトグル
+    menuBtn.classList.toggle('show');
+    globalNav.classList.toggle('show');
+    mask.classList.toggle('show');  
+}
+
+// 全てのトリガー要素をループ処理
+triggers.forEach(trigger => {
+    trigger.addEventListener('click', toggleMenu)
+})
